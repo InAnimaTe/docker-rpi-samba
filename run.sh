0f9fa5f2edb6b9133c315bdcd9cd2863e25b710e
petr.trnak <petr@petrol.local>
2014-09-29 11:38:55 +0200

    Fix starting stopped container.

--- run.sh
+++ run.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-set -e
 
 CONFIG_FILE="/etc/samba/smb.conf"
-useradd smbuser -M
 
-cat >"$CONFIG_FILE" <<EOT
+initialized=`getent passwd |grep -c '^smbuser:'`
+set -e
+if [ $initialized = "0" ]; then
+  useradd smbuser -M
+
+  cat >"$CONFIG_FILE" <<EOT
 [global]
 workgroup = WORKGROUP
 security = user
@@ -20,10 +23,10 @@ printcap name = /dev/null
 disable spoolss = yes
 EOT
 
-while getopts ":u:s:h" opt; do
-  case $opt in
-    h)
-      cat <<EOH
+  while getopts ":u:s:h" opt; do
+    case $opt in
+      h)
+        cat <<EOH
 Samba server container
 
 Container will be configured as samba sharing server and it just needs:
@@ -54,48 +57,50 @@ docker run -d -p 445:445 \\
   -s "Documents (readonly):/share/data/documents:ro:guest,alice,bob"
 
 EOH
-      exit 1
-      ;;
-    u)
-      echo -n "Add user "
-      IFS=: read username password <<<"$OPTARG"
-      echo -n "'$username' "
-      useradd "$username" -M
-      echo -n "with password '$password' "
-      echo "$password" |tee - |smbpasswd -s -a "$username"
-      echo "DONE"
-      ;;
-    s)
-      echo -n "Add share "
-      IFS=: read sharename sharepath readwrite users <<<"$OPTARG"
-      echo -n "'$sharename' "
-      echo "[$sharename]" >>"$CONFIG_FILE"
-      chown smbuser "$sharepath"
-      echo -n "path '$sharepath' "
-      echo "path = \"$sharepath\"" >>"$CONFIG_FILE"
-      echo -n "read"
-      if [[ "rw" = "$readwrite" ]] ; then
-        echo -n "+write "
-        echo "read only = no" >>"$CONFIG_FILE"
-      else
-        echo -n "-only "
-        echo "read only = yes" >>"$CONFIG_FILE"
-      fi
-      echo -n "for users: "
-      users=$(echo "$users" |tr "," " ")
-      echo -n "$users "
-      echo "valid users = $users" >>"$CONFIG_FILE"
-      echo "DONE"
-      ;;
-    \?)
-      echo "Invalid option: -$OPTARG"
-      exit 1
-      ;;
-    :)
-      echo "Option -$OPTARG requires an argument."
-      exit 1
-      ;;
-  esac
-done
+        exit 1
+        ;;
+      u)
+        echo -n "Add user "
+        IFS=: read username password <<<"$OPTARG"
+        echo -n "'$username' "
+        useradd "$username" -M
+        echo -n "with password '$password' "
+        echo "$password" |tee - |smbpasswd -s -a "$username"
+        echo "DONE"
+        ;;
+      s)
+        echo -n "Add share "
+        IFS=: read sharename sharepath readwrite users <<<"$OPTARG"
+        echo -n "'$sharename' "
+        echo "[$sharename]" >>"$CONFIG_FILE"
+        chown smbuser "$sharepath"
+        echo -n "path '$sharepath' "
+        echo "path = \"$sharepath\"" >>"$CONFIG_FILE"
+        echo -n "read"
+        if [[ "rw" = "$readwrite" ]] ; then
+          echo -n "+write "
+          echo "read only = no" >>"$CONFIG_FILE"
+        else
+          echo -n "-only "
+          echo "read only = yes" >>"$CONFIG_FILE"
+        fi
+        echo -n "for users: "
+        users=$(echo "$users" |tr "," " ")
+        echo -n "$users "
+        echo "valid users = $users" >>"$CONFIG_FILE"
+        echo "DONE"
+        ;;
+      \?)
+        echo "Invalid option: -$OPTARG"
+        exit 1
+        ;;
+      :)
+        echo "Option -$OPTARG requires an argument."
+        exit 1
+        ;;
+    esac
+  done
+
+fi
 
 exec ionice -c 3 smbd -FS --configfile="$CONFIG_FILE"
